257a6aca39cf7351876813cbd92c4d21f1de20d6
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2021-10-08 21:41:29 +0200

    not working

--- js/exhibits.js
+++ js/exhibits.js
@@ -1,11 +1,13 @@
-import { exhibitsArray } from "components/exhibitsList.js";
+import { exhibitsArray } from "./constants/exhibitsList.js";
+
 const exhibitsContainer = document.querySelector(".exhibits-container");
 
 
 function exhibitsOverview () {
+
     for(let i = 0; i < exhibitsArray.length; i++)
     exhibitsContainer.innerHTML = 
-    `<h2>${exhibitsArray.name}`
+    `<h2>${exhibitsArray.name}</h2>`;
 }
 
 exhibitsOverview();
